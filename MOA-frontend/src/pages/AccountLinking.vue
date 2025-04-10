3c0d8d0729ec6a3b8513b8a351e0f1f989d0a5fe
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-04-10 12:25:43 +0900

    feat[#38]: AccountPage
    : +연동된 계좌 리스트 표시
      -기능 연동

---
+++ MOA-frontend/src/pages/AccountLinking.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <span><img src="../assets/image 40.png" alt="" /></span>
+      <span
+        >모아찌 님의 계좌 통장을 흔들어서 부자되세요₩!@@!#!@#ㄸ$@ 현재까지 얼마 모아찌 ? -
+        100,000,000원
+      </span>
+      <span><img src="../assets/image 40.png" alt="" /></span>
+      <hr />
+    </div>
+    <div>
+      <div>
+        <span>국민은행</span><span>652321-43-231232</span>
+        <span>
+          <BaseButton _text="흔들기" _type="full"></BaseButton>
+        </span>
+        <span>
+          <BaseButton _text="연동 해지" _type=""></BaseButton>
+        </span>
+        <hr />
+      </div>
+    </div>
+    <div><BaseButton _text="연동하기" _type="full" _w="30rem"></BaseButton></div>
+  </div>
+</template>
+<script setup>
+import BaseButton from '@/components/common/BaseButton.vue'
+</script>
