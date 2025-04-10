f70193991cff1d1ca814a0d2d1f9837bac6a7b43
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-04-10 14:44:49 +0900

    feat[#38]: AccountPage
    : +이름과 모인잔돈을 변수로 받아 표시
      +input 추가
      +디자인 추가

--- MOA-frontend/src/pages/AccountLinking.vue
+++ MOA-frontend/src/pages/AccountLinking.vue
@@ -14,28 +14,89 @@
 
     <hr />
 
-    <!-- 은행 정보 + 버튼들 -->
-    <div class="d-flex justify-content-between align-items-center mb-4">
-      <div class="d-flex align-items-center gap-2">
-        <div>국민은행</div>
-        <div>652321-43-231232</div>
+    <!-- 은행 정보 리스트 -->
+    <div v-for="(account, idx) in accounts" :key="idx" class="mb-4">
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center gap-3 fs-5 fw-semibold">
+          <div>{{ account.bank }}</div>
+          <div>{{ account.number }}</div>
+        </div>
+
+        <div class="d-flex gap-2">
+          <BaseButton _text="흔들기" _type="fill" _w="3.5rem" _h="1.5rem" />
+          <BaseButton
+            @click="unlinkAccount(idx)"
+            _text="연동 해지"
+            _type=""
+            _w="4.5rem"
+            _h="1.5rem"
+          />
+        </div>
       </div>
 
-      <div class="d-flex gap-2">
-        <BaseButton _text="흔들기" _type="fill" _w="3.5rem" _h="1.5rem" />
-        <BaseButton _text="연동 해지" _type="" _w="4.5rem" _h="1.5rem" />
+      <!-- 마지막이 아닌 줄만 구분선 -->
+      <hr v-if="idx !== accounts.length - 1" />
+    </div>
+
+    <!-- 추가 입력 폼 (바로 아래) -->
+    <div v-if="showAddAccount" class="mt-4">
+      <div class="d-flex justify-content-center gap-2 mb-3">
+        <BaseInput _ph="은행명" _type="text" v-model="newBank" />
+        <BaseInput _ph="계좌번호" _type="text" v-model="newNumber" />
+      </div>
+      <div class="d-flex justify-content-center gap-2">
+        <BaseButton @click="addAccount" _text="추가" _type="fill" _w="5rem" />
+        <BaseButton @click="cancelAdd" _text="취소" _type="" _w="5rem" />
       </div>
     </div>
 
     <hr />
 
-    <!-- 맨 아래 버튼 -->
+    <!-- 맨 아래 '연동하기' 버튼 -->
     <div class="d-flex justify-content-center">
-      <BaseButton _text="연동하기" _type="fill" _w="30rem" />
+      <BaseButton @click="showAddAccount = true" _text="계좌 추가하기" _type="fill" _w="30rem" />
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import BaseButton from '@/components/common/BaseButton.vue'
+import BaseInput from '@/components/common/BaseInput.vue'
+
+const accounts = ref([{ bank: '국민은행', number: '652321-43-231232' }])
+
+const showAddAccount = ref(false)
+const newBank = ref('')
+const newNumber = ref('')
+
+const addAccount = () => {
+  if (newBank.value.trim() && newNumber.value.trim()) {
+    accounts.value.push({ bank: newBank.value, number: newNumber.value })
+    newBank.value = ''
+    newNumber.value = ''
+    showAddAccount.value = false
+  } else {
+    alert('은행명과 계좌번호를 모두 입력해주세요!')
+  }
+}
+
+const unlinkAccount = idx => {
+  accounts.value.splice(idx, 1)
+}
+
+const cancelAdd = () => {
+  newBank.value = ''
+  newNumber.value = ''
+  showAddAccount.value = false
+}
 </script>
+
+<style scoped>
+hr {
+  border: none;
+  height: 1px;
+  background-color: #ccc;
+  margin: 1rem 0;
+}
+</style>
